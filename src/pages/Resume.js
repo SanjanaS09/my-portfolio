9e0efa7bbc0d0aa5734c77eb4c038b69dec60147
Sanjana <shettysanjana09@gmail.com>
2024-08-31 13:38:30 +0530

    Firebase

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import './Resume.css';
 
+/**
+ * Resume component
+ *
+ * Renders a resume page with an iframe to display the resume PDF and a download link.
+ */
 function Resume() {
   return (
     <div className="resume">
       <h1>Resume</h1>
-      <a href="Sanjana Resume.pdf" download className="download-link">Download My Resume</a>
+      <div className="iframe-container">
+        <iframe
+          src="Sanjana Resume.pdf"
+          width="1200"
+          height="800"
+          title="Sanjana Shetty"
+        />
+      </div>
+      <div className="download-link-container">
+        <a
+          href="Sanjana Resume.pdf"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="download-link"
+        >
+          Download My Resume
+        </a>
+      </div>
     </div>
   );
 }
